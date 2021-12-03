d83c67e7055f8196d5c9b1e9867c2b12e6ba2d27
Jan Feil <11638228+jfeil@users.noreply.github.com>
2021-12-03 20:03:28 +0100

    Simplified code and first radio buttons

--- src/document_builder.py
+++ src/document_builder.py
@@ -1,4 +1,3 @@
-import logging
 from typing import List
 
 from reportlab.lib.units import inch, mm
@@ -12,6 +11,10 @@ from src.datatypes import Question, MultipleChoice
 
 PAGE_HEIGHT = defaultPageSize[1];
 PAGE_WIDTH = defaultPageSize[0]
+linespacing = 14
+min_lines = 3
+space_between = 0.2 * linespacing
+space_bottom = 0.4 * linespacing
 
 
 class QuestionFlowable(Flowable):
@@ -21,42 +24,74 @@ class QuestionFlowable(Flowable):
                  fontSize=9, solution: bool = False, x=0, y=0, width=4 / 5 * PAGE_WIDTH):
         super().__init__()
 
-        self.linespacing = 14
-        self.min_lines = 3
+        def answer_letter(index):
+            return "abc"[index]
 
         self.question_index = question_index
         self.question = question
         self.mchoice = mchoice
         self.solution = solution
+
+        self.paragraph_style = ParagraphStyle('DefaultStyle', fontName=fontName, fontSize=fontSize)
+
         self.x = x
         self.y = y
         self.width = width
-        self.paragraph_style = ParagraphStyle('DefaultStyle', fontName=fontName, fontSize=fontSize)
 
-        lines_question = len(
-            simpleSplit(f"{self.question_index}. {self.question.question}", fontName, fontSize, self.width))
-        self.lines_answer = len(simpleSplit(self.question.answer_text, fontName, fontSize, 9 / 10 * self.width))
+        self.question_text = f"{self.question_index}. {self.question.question}"
+
+        if self.question.answer_index == -1:
+            self.answer_text = self.question.answer_text
+        else:
+            answer_letter = answer_letter(self.question.answer_index)
+            self.answer_text = f"{answer_letter}) {self.question.answer_text}"
+
+        lines_question = len(simpleSplit(self.question_text, fontName, fontSize, self.width))
+        self.lines_answer = len(simpleSplit(self.answer_text, fontName, fontSize, 9 / 10 * self.width))
 
         logging.debug(question_index, lines_question, self.lines_answer)
 
-        self.height_answer = max(self.lines_answer, self.min_lines) * self.linespacing
-        self.height_question = lines_question * self.linespacing
-        self.height = self.height_answer + 0.2 * self.linespacing + self.height_question + 0.4 * self.linespacing
+        self.height_answer = max(self.lines_answer, min_lines) * linespacing
+        self.height_question = lines_question * linespacing
+        self.height = self.height_answer + space_between + self.height_question + space_bottom
 
     def draw(self):
-        question = Paragraph(f"{self.question_index}. {self.question.question}", self.paragraph_style)
+        question = Paragraph(self.question_text, self.paragraph_style)
         question.wrapOn(self.canv, self.width, self.height)
-        question.drawOn(self.canv, self.x, 0.2 * self.linespacing + self.height_answer + 0.4 * self.linespacing)
+        question.drawOn(self.canv, self.x, space_between + self.height_answer + space_bottom)
 
         if self.solution:
-            solution = Paragraph(f"{self.question.answer_text}", self.paragraph_style)
+            solution = Paragraph(self.answer_text, self.paragraph_style)
             solution.wrapOn(self.canv, 9 / 10 * self.width, self.height)
             solution.drawOn(self.canv, self.x + 4 * mm,
-                            self.y + 0.3 * self.linespacing + max(self.min_lines - self.lines_answer,
-                                                                  0) * self.linespacing)
-        else:
-            self.canv.acroForm.textfieldRelative(x=self.x + 4 * mm, y=self.y + 0.3 * self.linespacing,
-                                                 width=9 / 10 * self.width, height=self.height_answer)
+                            self.y + space_bottom + max(min_lines - self.lines_answer, 0) * linespacing)
+        elif not self.solution and self.question.answer_index != -1:
+            radio_group = f"Question_{self.question_index}"
+            radio_size = 20
+            radio_1 = self.canv.acroForm.radio("test1", relative=True, size=radio_size, name=radio_group, x=self.x,
+                                               y=self.y + space_bottom + max(min_lines - self.lines_answer,
+                                                                             0) * linespacing)
+            solution = Paragraph("Radio button 1", self.paragraph_style)
+            solution.wrapOn(self.canv, 9 / 10 * self.width, self.height)
+            solution.drawOn(self.canv, self.x + 4 * mm,
+                            self.y + space_bottom + max(min_lines - self.lines_answer, 0) * linespacing)
+
+            radio_2 = self.canv.acroForm.radio("test2", relative=True, size=radio_size, name=radio_group, x=self.x,
+                                               y=self.y + space_bottom + max(min_lines - self.lines_answer,
+                                                                             0) * linespacing + radio_size)
+            solution = Paragraph("Radio button 2", self.paragraph_style)
+            solution.wrapOn(self.canv, 9 / 10 * self.width, self.height)
+            solution.drawOn(self.canv, self.x + 4 * mm,
+                            self.y + radio_size + space_bottom + max(min_lines - self.lines_answer, 0) * linespacing)
+
+        elif not self.solution and self.question.answer_index == -1:
+            # FieldFlags are:
+            # 1 << 1: required
+            # 1 << 12: MultiLine
+            self.canv.acroForm.textfieldRelative(x=self.x + 4 * mm, y=self.y + space_bottom, width=9 / 10 * self.width,
+                                                 height=self.height_answer, fontName=self.paragraph_style.fontName,
+                                                 fontSize=self.paragraph_style.fontSize, maxlen=None,
+                                                 fieldFlags=(1 << 1) + (1 << 12))
 
 
 def go():
