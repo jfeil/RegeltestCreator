bbfdfd1ea41073c2207622b83452a7edcc207b0e
Jan Feil <11638228+jfeil@users.noreply.github.com>
2021-12-09 22:02:23 +0100

    Renamed ui file

--- src/ui_mainwindow.py
+++ src/ui_mainwindow.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 ################################################################################
-## Form generated from reading UI file 'MainWindow.ui'
+## Form generated from reading UI file 'mainwindow.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.2.2
 ##
