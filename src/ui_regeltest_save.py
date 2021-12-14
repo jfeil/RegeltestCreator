05ba56765bdc6bc51d7e4328d33e6b2e305e5c64
Jan Feil <11638228+jfeil@users.noreply.github.com>
2021-12-14 22:18:42 +0100

    Improve save dialog

--- src/ui_regeltest_save.py
+++ src/ui_regeltest_save.py
@@ -8,33 +8,34 @@
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QMetaObject, Qt)
-from PySide6.QtWidgets import (QDialogButtonBox,
-                               QGridLayout, QHBoxLayout, QLabel, QLineEdit,
-                               QPushButton, QSizePolicy, QSpacerItem, QWidget)
-
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QGridLayout, QHBoxLayout, QLabel, QLineEdit,
+    QPushButton, QSizePolicy, QSpacerItem, QWidget)
 
 class Ui_RegeltestSave(object):
     def setupUi(self, RegeltestSave):
         if not RegeltestSave.objectName():
             RegeltestSave.setObjectName(u"RegeltestSave")
         RegeltestSave.resize(470, 171)
+        RegeltestSave.setFocusPolicy(Qt.NoFocus)
         self.gridLayout = QGridLayout(RegeltestSave)
         self.gridLayout.setObjectName(u"gridLayout")
-        self.label_3 = QLabel(RegeltestSave)
-        self.label_3.setObjectName(u"label_3")
-
-        self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
-
-        self.label_2 = QLabel(RegeltestSave)
-        self.label_2.setObjectName(u"label_2")
-
-        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
-
         self.label = QLabel(RegeltestSave)
         self.label.setObjectName(u"label")
 
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+
+        self.label_3 = QLabel(RegeltestSave)
+        self.label_3.setObjectName(u"label_3")
+
+        self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
 
         self.widget = QWidget(RegeltestSave)
         self.widget.setObjectName(u"widget")
@@ -50,14 +51,13 @@ class Ui_RegeltestSave(object):
 
         self.horizontalLayout.addWidget(self.icon_edit_button)
 
-        self.gridLayout.addWidget(self.widget, 1, 1, 1, 1)
 
-        self.buttonBox = QDialogButtonBox(RegeltestSave)
-        self.buttonBox.setObjectName(u"buttonBox")
-        self.buttonBox.setOrientation(Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+        self.gridLayout.addWidget(self.widget, 1, 2, 1, 1)
 
-        self.gridLayout.addWidget(self.buttonBox, 5, 0, 1, 2)
+        self.label_2 = QLabel(RegeltestSave)
+        self.label_2.setObjectName(u"label_2")
+
+        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
 
         self.widget_2 = QWidget(RegeltestSave)
         self.widget_2.setObjectName(u"widget_2")
@@ -73,30 +73,53 @@ class Ui_RegeltestSave(object):
 
         self.horizontalLayout_2.addWidget(self.output_edit_button)
 
-        self.gridLayout.addWidget(self.widget_2, 3, 1, 1, 1)
 
-        self.title_edit = QLineEdit(RegeltestSave)
-        self.title_edit.setObjectName(u"title_edit")
-
-        self.gridLayout.addWidget(self.title_edit, 0, 1, 1, 1)
+        self.gridLayout.addWidget(self.widget_2, 3, 2, 1, 1)
 
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.gridLayout.addItem(self.verticalSpacer, 4, 0, 1, 1)
 
+        self.buttonBox = QDialogButtonBox(RegeltestSave)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel|QDialogButtonBox.Ok)
+
+        self.gridLayout.addWidget(self.buttonBox, 5, 0, 1, 3)
+
+        self.widget_3 = QWidget(RegeltestSave)
+        self.widget_3.setObjectName(u"widget_3")
+        self.widget_3.setFocusPolicy(Qt.StrongFocus)
+        self.horizontalLayout_3 = QHBoxLayout(self.widget_3)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.title_edit = QLineEdit(self.widget_3)
+        self.title_edit.setObjectName(u"title_edit")
+
+        self.horizontalLayout_3.addWidget(self.title_edit)
+
+
+        self.gridLayout.addWidget(self.widget_3, 0, 2, 1, 1)
+
+        QWidget.setTabOrder(self.title_edit, self.icon_path_edit)
+        QWidget.setTabOrder(self.icon_path_edit, self.icon_edit_button)
+        QWidget.setTabOrder(self.icon_edit_button, self.output_edit)
+        QWidget.setTabOrder(self.output_edit, self.output_edit_button)
+        QWidget.setTabOrder(self.output_edit_button, self.widget_3)
+
         self.retranslateUi(RegeltestSave)
         self.buttonBox.accepted.connect(RegeltestSave.accept)
         self.buttonBox.rejected.connect(RegeltestSave.reject)
+        RegeltestSave.windowTitleChanged.connect(self.title_edit.setFocus)
 
         QMetaObject.connectSlotsByName(RegeltestSave)
-
     # setupUi
 
     def retranslateUi(self, RegeltestSave):
-        RegeltestSave.setWindowTitle(QCoreApplication.translate("RegeltestSave", u"Dialog", None))
-        self.label_3.setText(QCoreApplication.translate("RegeltestSave", u"Output", None))
-        self.label_2.setText(QCoreApplication.translate("RegeltestSave", u"Icon pfad", None))
+        RegeltestSave.setWindowTitle(QCoreApplication.translate("RegeltestSave", u"Save Regeltest", None))
         self.label.setText(QCoreApplication.translate("RegeltestSave", u"Titel", None))
-        self.icon_edit_button.setText(QCoreApplication.translate("RegeltestSave", u"FileDialog", None))
-        self.output_edit_button.setText(QCoreApplication.translate("RegeltestSave", u"FileDialog", None))
+        self.label_3.setText(QCoreApplication.translate("RegeltestSave", u"Output", None))
+        self.icon_edit_button.setText(QCoreApplication.translate("RegeltestSave", u"Select icon", None))
+        self.label_2.setText(QCoreApplication.translate("RegeltestSave", u"Icon pfad", None))
+        self.output_edit_button.setText(QCoreApplication.translate("RegeltestSave", u"Save as", None))
     # retranslateUi
+
