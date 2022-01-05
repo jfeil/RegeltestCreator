49aced0fee4158f8b474c93db27f9a79c927e06f
Jan Feil <11638228+jfeil@users.noreply.github.com>
2022-01-05 21:34:48 +0100

    Update build_executable_macos.sh

--- build_executable_macos.sh
+++ build_executable_macos.sh
@@ -1,2 +1,2 @@
 pyinstaller RegeltestCreator.pyw -F --osx-bundle-identifier de.jfeil.RegeltestCreator --windowed
-zip dist/RegeltestCreator.app.zip dist/RegeltestCreator.app
+zip -r dist/RegeltestCreator.app.zip dist/RegeltestCreator.app
