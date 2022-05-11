1fcdc08d8f90208234521d000706f4a14cd70d9f
Jan Feil <11638228+jfeil@users.noreply.github.com>
2022-05-11 08:12:04 +0200

    Clean CollapseButton

--- src/custom_ui.py
+++ src/custom_ui.py
@@ -2,39 +2,7 @@ from typing import Optional
 
 from PySide6.QtCore import QPropertyAnimation, QEasingCurve, QAbstractAnimation
 from PySide6.QtGui import Qt
-from PySide6.QtWidgets import QToolButton, QApplication, QWidget
-
-"""
-class CollapseButton : public QToolButton {
-public:
-  CollapseButton(QWidget *parent) : QToolButton(parent), content_(nullptr) {
-    setCheckable(true);
-    setStyleSheet("background:none");
-    setIconSize(QSize(8, 8));
-    setFont(QApplication::font());
-    connect(this, &QToolButton::toggled, [=](bool checked) {
-      setArrowType(checked ? Qt::ArrowType::DownArrow : Qt::ArrowType::RightArrow);
-      content_ != nullptr && checked ? showContent() : hideContent();
-    });
-  }
-
-  void setText(const QString &text) {
-    QToolButton::setText(" " + text);
-  }
-
-  void setContent(QWidget *content) {
-  }
-
-  void hideContent() {
-    animator_.setDirection(QAbstractAnimation::Backward);
-    animator_.start();
-  }
-
-  void showContent() {
-    animator_.setDirection(QAbstractAnimation::Forward);
-    animator_.start();
-  }
-"""
+from PySide6.QtWidgets import QToolButton, QWidget
 
 
 class CollapseButton(QToolButton):
@@ -47,29 +15,12 @@ class CollapseButton(QToolButton):
 
         self.animation = None  # type: Optional[QPropertyAnimation]
         self.content = None  # type: Optional[QWidget]
-        self.setFont(QApplication.font())
         self.toggled.connect(self.toggle_function)
 
     def setText(self, text: str) -> None:
         super().setText(" " + text)
 
     def set_content(self, content: QWidget):
-        """
-        assert(content != nullptr);
-        content_ = content;
-        auto animation_ = new QPropertyAnimation(content_, "maximumHeight"); // QObject with auto delete
-        animation_->setStartValue(0);
-        animation_->setEasingCurve(QEasingCurve::InOutQuad);
-        animation_->setDuration(300);
-        animation_->setEndValue(content->geometry().height() + 10);
-        animator_.addAnimation(animation_);
-        if (!isChecked()) {
-          content->setMaximumHeight(0);
-        }
-
-        :param content: 
-        :return: 
-        """
         self.content = content
         self.animation = QPropertyAnimation(content, b"maximumHeight")
         self.animation.setStartValue(0)
@@ -86,13 +37,6 @@ class CollapseButton(QToolButton):
             self.content.setMaximumHeight(new_end_value)
 
     def toggle_function(self, checked: bool):
-        """
-        setArrowType(checked ? Qt::ArrowType::DownArrow : Qt::ArrowType::RightArrow);
-        content_ != nullptr && checked ? showContent() : hideContent();
-
-        :param checked:
-        :return:
-        """
         self.setArrowType(Qt.DownArrow if checked else Qt.RightArrow)
         if checked:
             self.show_content()
@@ -100,17 +44,9 @@ class CollapseButton(QToolButton):
             self.hide_content()
 
     def hide_content(self):
-        """
-        animator_.setDirection(QAbstractAnimation::Backward);
-        animator_.start();
-        """
         self.animation.setDirection(QAbstractAnimation.Backward)
         self.animation.start()
 
     def show_content(self):
-        """
-        animator_.setDirection(QAbstractAnimation::Forward);
-        animator_.start();
-        """
         self.animation.setDirection(QAbstractAnimation.Forward)
         self.animation.start()
